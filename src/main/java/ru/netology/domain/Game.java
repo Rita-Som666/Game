2185038ebe5276976b1db7ed666aea794938c538
Tatiana Titova <ritasom666@gmail.com>
2024-03-07 20:40:55 +0400

    start hw

---
+++ src/main/java/ru/netology/domain/Game.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+import java.util.ArrayList;
+import java.util.List;
+public class Game {
+
+    public boolean register(Player player) {
+        return true;
+    }
+
+    List <Player> players = new ArrayList<>();
+
+
+}
